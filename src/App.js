4a59c70a25f08e3d6f0c18ce267ad41a795f3593
zuber57 <zubarahmad48@gmail.com>
2022-06-12 13:36:12 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import ProductList from "./components/ProuductList";
+import CheckoutAd from "./components/CheckoutAd";
+import ShoppingBasket from "./components/ShoppingBasket";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <Header/>
+    <Banner/>
+    {/* <CheckoutAd/>
+    <ShoppingBasket/> */}
+    <ProductList/>
+  </div>  );
+    
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return ( <header>
+  <div className="Header_logo"></div>
+  <div className="Header_search"></div>
+  <div className="Header_nav"></div>
+
+    </header>
+    )
+}
+export default Header;
