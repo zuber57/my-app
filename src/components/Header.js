5c0750665f2f94a2bd73b3a72a4bc07cf6fc9e0f
zuber57 <zubarahmad48@gmail.com>
2022-06-12 13:37:08 +0530

    -

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,31 @@
-const Header = () => {
-    return ( <header>
-  <div className="Header_logo"></div>
-  <div className="Header_search"></div>
-  <div className="Header_nav"></div>
+import React from "react"
+import './header.scss';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
 
+const Header = () => {
+  return(
+    <header className="header">
+      <div className="header__logo">
+        <AddBusinessIcon fontSize="large"/>&nbsp;
+        <span className="header__logoName">E-shop</span>
+      </div>
+      <div className="header__search">
+        <input />
+        <SearchIcon fontSize="small" className="header__searchIcon"/>
+      </div>
+      <div className="header__nav">
+        <div className="header__nav__user">
+          <span className="header__nav__lineOne">Hello Guest</span>
+          <span className="header__nav__lineTwo">Sign In</span>
+        </div>
+        <div className="header__nav__itemBasket">
+          <ShoppingCartIcon fontSize="small"/>&nbsp;Cart
+        </div>
+      </div>
     </header>
-    )
+  )
 }
+
 export default Header;
